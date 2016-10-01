54b37f0bd241e7bd83a796d80dcbc3683c5c70cb
Rajesh Babu <trcrajesh_be@yahoo.com>
2016-10-01 11:03:17 -0400

    Add files via upload

---
+++ src/app/sample-app.module.ts
@@ -0,0 +1,60 @@
+import {NgModule}      from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {
+  DevToolsExtension,
+  NgRedux
+} from 'ng2-redux';
+import {NgReduxRouter} from 'ng2-redux-router';
+import {
+  routing,
+  appRoutingProviders
+} from './sample-app.routing';
+import {
+  FormsModule,
+  FormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {RioSampleApp} from './sample-app';
+import {SessionActions} from '../actions/session.actions';
+import {SessionEpics} from '../epics/session.epics';
+import {
+  RioAboutPage,
+  RioCounterPage
+} from '../pages';
+import {RioCounter} from '../components/counter/counter.component';
+import {RioLoginModule} from '../components/login/login.module';
+import {RioUiModule} from '../components/ui/ui.module';
+import {RioNavigatorModule} from '../components/navigator/navigator.module';
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    routing,
+    CommonModule,
+    RioLoginModule,
+    RioUiModule,
+    RioNavigatorModule
+  ],
+  declarations: [
+    RioSampleApp,
+    RioAboutPage,
+    RioCounterPage,
+    RioCounter
+  ],
+  bootstrap: [
+    RioSampleApp
+  ],
+  providers: [
+    DevToolsExtension,
+    FormBuilder,
+    NgRedux,
+    NgReduxRouter,
+    appRoutingProviders,
+    SessionActions,
+    SessionEpics
+  ]
+})
+export class RioSampleAppModule { }
